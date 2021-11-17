6660fffb0d96012008050b392e971f9a43892c1c
Lucas Corrêa <lucas.dcorrea1@gmail.com>
2021-11-16 22:53:56 -0300

    Init project

---
+++ src/NerdStore.Catalogo.Domain/Produto.cs
@@ -0,0 +1,16 @@
+﻿using NerdStore.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public class Produto : Entity, IAggregateRoot
+    {
+        public string Nome { get; private set; }
+    }
+
+    public class Categoria : Entity
+    {
+    }
+}
